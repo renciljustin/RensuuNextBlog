a7f882e0d244eda30a3d9c62306ae361783f09d6
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-11 00:53:57 +0800

    Add custom fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,46 +2,47 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layouts/Layout';
 import localFont from '@next/font/local';
+import NavigationBar from '../components/layouts/NavigationBar';
 
-const inter = localFont({
-  src: [
-    {
-      path: './Inter-ExtraLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: './Inter-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './Inter-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-  ],
-});
+// const inter = localFont({
+//   src: [
+//     {
+//       path: './Inter-ExtraLight.woff2',
+//       weight: '200',
+//       style: 'normal',
+//     },
+//     {
+//       path: './Inter-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: './Inter-SemiBold.woff2',
+//       weight: '600',
+//       style: 'normal',
+//     },
+//   ],
+// });
 
-const sofiaSansExtraCondensed = localFont({
-  src: [
-    {
-      path: './SofiaSansExtraCondensed-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './SofiaSansExtraCondensed-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-  ],
-});
+// const sofiaSansExtraCondensed = localFont({
+//   src: [
+//     {
+//       path: './SofiaSansExtraCondensed-Regular.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: './SofiaSansExtraCondensed-SemiBold.woff2',
+//       weight: '600',
+//       style: 'normal',
+//     },
+//   ],
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <style jsx global>{`
+      {/* <style jsx global>{`
         html {
           font-family: ${inter.style.fontFamily};
         }
@@ -54,7 +55,8 @@ export default function App({ Component, pageProps }: AppProps) {
         h6 {
           font-family: ${sofiaSansExtraCondensed.style.fontFamily};
         }
-      `}</style>
+      `}</style> */}
+      <NavigationBar />
       <Component {...pageProps} />
     </Layout>
   );
