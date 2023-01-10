e229130056c58764b4cab3c6c3d25a7019cd7af2
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-11 04:02:30 +0800

    Add Hero component

---
+++ components/home/Hero.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import Container from '../layouts/Container';
+import Section from '../layouts/Section';
+import classes from './Hero.module.css';
+import Typewriter from 'typewriter-effect';
+
+export default function Hero() {
+  const typewriter = (
+    <Typewriter
+      options={{
+        strings: [`I'm a code wizard.`, 'I turn coffee into code.'],
+        autoStart: true,
+        loop: true,
+        delay: 50,
+        deleteSpeed: 100,
+      }}
+    />
+  );
+  return (
+    <Section>
+      <Container>
+        <Image
+          src={'/images/profile.jpg'}
+          alt={'My profile'}
+          height={200}
+          width={200}
+          className={classes.profilePhoto}
+        />
+        <div className={classes.circles}>
+          <div
+            className={[classes.circle, classes.circleGreen].join(' ')}
+          ></div>
+        </div>
+        <h1 className={classes.greetings}>
+          Hi I&apos;m RJ.&nbsp;
+          <span className={classes.typewriter}>{typewriter}</span>
+        </h1>
+      </Container>
+    </Section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
-import Container from '../components/layouts/Container';
+import Hero from '../components/home/Hero';
 
 export default function HomePage() {
-  return (
-    <Container>
-      <h1>Hi I&apos;m RJ and welcome to my NEXT blog</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam non
-        quibusdam modi numquam cumque adipisci deserunt animi vero fugit
-        pariatur fugiat, commodi aspernatur corporis, quia, sint iusto tempora
-        et tenetur!
-      </p>
-    </Container>
-  );
+  return <Hero />;
 }
