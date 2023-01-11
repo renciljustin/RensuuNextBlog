815e23b6ed1f2e74a8d07e38176c87a617e7350a
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-12 01:02:33 +0800

    Add dynamic header in Section layout
    Add dynamic title and description in Section header layout.

--- components/layouts/Section.tsx
+++ components/layouts/Section.tsx
@@ -1,5 +1,21 @@
 import classes from './Section.module.css';
 
-export default function Section({ children }: React.PropsWithChildren) {
-  return <section className={classes.section}>{children}</section>;
+export default function Section({
+  children,
+  title,
+  description,
+}: {
+  children: JSX.Element;
+  title?: string;
+  description?: string;
+}) {
+  return (
+    <section className={classes.section}>
+      <div className={classes.header}>
+        {title && <h2 className={classes.title}>{title}</h2>}
+        {description && <p className={classes.description}>{description}</p>}
+      </div>
+      {children}
+    </section>
+  );
 }
