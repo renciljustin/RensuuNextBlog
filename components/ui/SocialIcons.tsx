aa359726ebda6c20c53962839f65382d91d2847f
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-13 01:20:32 +0800

    Add Social Icon component

---
+++ components/ui/SocialIcons.tsx
@@ -0,0 +1,41 @@
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillFileUnknown,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+import classes from './SocialIcons.module.css';
+
+export default function SocialIcons() {
+  return (
+    <div className={classes.container}>
+      <a
+        href="https://facebook.com/renciljustin"
+        target={'_blank'}
+        rel="noopener noreferrer"
+      >
+        <AiFillFacebook
+          className={[classes.socialIcon, classes.iconFacebook].join(' ')}
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/renciljustinevangelista/"
+        target={'_blank'}
+        rel="noopener noreferrer"
+      >
+        <AiFillLinkedin
+          className={[classes.socialIcon, classes.iconLinkedIn].join(' ')}
+        />
+      </a>
+      <a
+        href="https://github.com/renciljustin"
+        target={'_blank'}
+        rel="noopener noreferrer"
+      >
+        <AiFillGithub
+          className={[classes.socialIcon, classes.iconGithub].join(' ')}
+        />
+      </a>
+    </div>
+  );
+}
