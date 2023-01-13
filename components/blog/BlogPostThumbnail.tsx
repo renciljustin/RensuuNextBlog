6764a8b2a11bb1a39f2a38abcd85dc006830e39e
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-14 03:31:22 +0800

    Update Blog Post Thumbnail component
    Add routing when Image or Title element was clicked.

--- components/blog/BlogPostThumbnail.tsx
+++ components/blog/BlogPostThumbnail.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { PostData } from '../../data/models/post-data';
 import Pill from '../ui/Pill';
 import PillGroup from '../ui/PillGroup';
@@ -6,21 +7,29 @@ import classes from './BlogPostThumbnail.module.css';
 
 export default function BlogPostThumbnail({ post }: { post: PostData }) {
   return (
-    <div>
-      <Image
-        src={`/images/blogs/${post.image}`}
-        alt={'My profile'}
-        height={225}
-        width={400}
-        className={classes.photo}
-      />
+    <div className={classes.container}>
+      <Link href={`/blog-posts/${post.slug}`}>
+        <Image
+          src={`/images/blogs/${post.image}`}
+          alt={'My profile'}
+          height={225}
+          width={400}
+          className={classes.photo}
+        />
+      </Link>
       <div className={classes.details}>
         <PillGroup>
           {post.tags.map((tag) => (
-            <Pill key={tag} label={'#' + tag} destination={'/'} />
+            <Pill
+              key={tag}
+              label={'#' + tag}
+              destination={'/blog-posts/categories/' + tag}
+            />
           ))}
         </PillGroup>
-        <h3 className={classes.title}>{post.title}</h3>
+        <Link href={`/blog-posts/${post.slug}`} className={classes.link}>
+          <h3 className={classes.title}>{post.title}</h3>
+        </Link>
         <article className={classes.description}>{post.excerpt}</article>
         <footer className={classes.footer}>
           <span className={classes.author}>{post.author}</span>
