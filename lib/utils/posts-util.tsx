9ffa6cd29e73169688cf731df0b71f9e7183148e
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-14 01:54:38 +0800

    Update posts util.

--- lib/utils/posts-util.tsx
+++ lib/utils/posts-util.tsx
@@ -5,15 +5,24 @@ import matter from 'gray-matter';
 import { PostData } from '../../data/models/post-data';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
+const postCategories = [
+  'ExpressJS',
+  'Info',
+  'MongoDB',
+  'NextJS',
+  'ReactJS',
+  'Typescript',
+  'Vercel',
+];
 
 export function getPostsFiles(): string[] {
   return fs.readdirSync(postsDirectory);
 }
 
-export function getPostData(postIdentifier: string): PostData {
-  const postSlug = postIdentifier.replace(/\.md$/, ''); // removes the file extension
-  const filePath = path.join(postsDirectory, `${postSlug}.md`);
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
+export function getPostData(id: string): PostData {
+  const postSlug = getPostSlug(id);
+  const filePath = getFilePath(postsDirectory, postSlug);
+  const fileContent = getFileContent(filePath);
   const { data, content } = matter(fileContent);
 
   const postData = {
@@ -25,11 +34,40 @@ export function getPostData(postIdentifier: string): PostData {
   return postData;
 }
 
+export function getPartialPostData(id: string): PostData {
+  const postSlug = getPostSlug(id);
+  const filePath = getFilePath(postsDirectory, postSlug);
+  const fileContent = getFileContent(filePath);
+  const { data } = matter(fileContent);
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+  } as PostData;
+
+  return postData;
+}
+
+function getPostSlug(id: string): string {
+  const postSlug = id.replace(/\.md$/, ''); // removes the file extension
+  return postSlug;
+}
+
+function getFilePath(dir: string, fileName: string): string {
+  const filePath = path.join(dir, `${fileName}.md`);
+  return filePath;
+}
+
+function getFileContent(filePath: string) {
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  return fileContent;
+}
+
 export function getAllPosts(): PostData[] {
   const postFiles = getPostsFiles();
 
   const allPosts = postFiles.map((postFile) => {
-    return getPostData(postFile);
+    return getPartialPostData(postFile);
   });
 
   const sortedPosts = allPosts.sort((postA, postB) =>
@@ -45,4 +83,11 @@ export function getFeaturedPosts(): PostData[] {
   const filteredPosts = allPosts.filter((post) => post.isFeatured);
 
   return filteredPosts;
+}
+
+export function getAllCategories(): string[] {
+  const sortedPostCategories = postCategories.sort((catA, catB) =>
+    catA > catB ? 1 : -1
+  );
+  return sortedPostCategories;
 }
