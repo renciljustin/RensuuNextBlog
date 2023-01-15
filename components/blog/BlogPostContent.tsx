9577d88d2ec0f2c99007b90b3c9e17d5bbcc24f3
Rencil Justin Evangelista <renciljustinevangelista@gmail.com>
2023-01-16 02:45:51 +0800

    Configure syntax highlighter

--- components/blog/BlogPostContent.tsx
+++ components/blog/BlogPostContent.tsx
@@ -3,8 +3,17 @@ import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { PostData } from '../../data/models/post-data';
 import classes from './BlogPostContent.module.css';
 import BlogPostContentHeader from './BlogPostContentHeader';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import ts from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import Link from 'next/link';
 
 export default function BlogPostContent({ post }: { post: PostData }) {
+  SyntaxHighlighter.registerLanguage('js', js);
+  SyntaxHighlighter.registerLanguage('ts', ts);
+  SyntaxHighlighter.registerLanguage('jsx', jsx);
   const customRenderers = {
     p(paragraph: any) {
       const { node } = paragraph;
@@ -24,6 +33,29 @@ export default function BlogPostContent({ post }: { post: PostData }) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code: any) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter style={atomDark} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+
+    a(link: any) {
+      const { href, children, title } = link;
+
+      if (title && title === 'pageLink')
+        return <Link href={href}>{children}</Link>;
+
+      return (
+        <a href={href} target="_blank" rel="noreferrer noopener">
+          {children}
+        </a>
+      );
     },
   };
   return (
